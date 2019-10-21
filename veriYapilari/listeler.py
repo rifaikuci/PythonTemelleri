9dda3a9e7faca57cc27690b8ec74553a0298798a
rifai <rfai4747@hotmail.com>
2019-10-21 22:30:55 +0300

    liste işlemleri

---
+++ veriYapilari/listeler.py
@@ -0,0 +1,30 @@
+#********** Diziler Referans Tiptir . *****
+
+# Liste tanımlama  sytax'ı
+
+# myList = [] # empty list
+#
+# myList2 =  [1,"Deneme", 0.60]
+#
+# print(myList2.index(0.60))
+
+sehirler = ["Rifai" ,"Hifa", "Betül", "Ahmet","Betül"]
+
+sehirler.append("Mehmet")
+sehirler.remove("Rifai")
+#
+denemeList = (("rİFAİ","kUÇİ"))
+
+print(sehirler.count("Betül")) # listede kaç tane olduğunu döndürür.
+print(sehirler.pop(0))
+sehirler.insert(0,"Mardin")
+sehirler.reverse()
+print(sehirler)
+
+sehirler3 = sehirler.copy()
+
+sehirler3.append("Kırşehir")
+sehirler.extend(sehirler3) #sehirler3'e sehirlere eklemesi yapılır.
+sehirler.sort() # sıralamayı yapar
+sehirler.reverse()  #ters çevirme işlemini yapar
+print(sehirler)
